654a478926eff39efefd7ffa2c34bb2fe6f72b81
Terrence Howard <chemisus@gmail.com>
2013-03-19 15:07:20 -0400

    added AdvancedRoute and test for it

---
+++ Shoestring/AdvancedRoute.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Shoestring;
+
+use Bootstrap\Route;
+
+class AdvancedRoute implements Route
+{
+
+    private $route;
+
+    public function __construct($method, $path, $callback)
+    {
+        $this->route = new BasicRoute($method, $this->translate($path), $callback);
+    }
+
+    /**
+     * 
+     * @param type $value
+     * @return type
+     */
+    public function translate($value)
+    {
+        return strtr($value, [
+            '/' => '\/',
+            '.' => '\.',
+            '[' => '(',
+            ']' => ')?',
+            '<' => '(?P<',
+            '>' => '>.+?)',
+        ]);
+    }
+
+    public function execute(\Bootstrap\Request $request)
+    {
+        return $this->route->execute($request);
+    }
+
+}
